5eff9e148997e46602d7e71c00a771d7ebfaa669
erica <egucciar@gmail.com>
2018-02-04 19:56:36 -0500

    "working" with issues

--- src/moduleImp/canvas.js
+++ src/moduleImp/canvas.js
@@ -8,6 +8,10 @@ function fitToContainer(canvas) {
     canvas.height = canvas.offsetHeight;
 }
 
+const shapeMap = {};
+
+let id = 0;
+
 /**
  * 
  * @param {*} x the starting X coordinate
@@ -31,7 +35,7 @@ function shape(ctx, x = 0, y = 0, w = 1, h = 1, fill = '#000000') {
 
     function clear() {
         ctx.clearRect(internalX, internalY, internalW, internalH);
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        // ctx.clearRect(0, 0, canvasWidth, canvasHeight);
         ctx.beginPath();
     }
 
@@ -55,18 +59,53 @@ function shape(ctx, x = 0, y = 0, w = 1, h = 1, fill = '#000000') {
         }
     }
 
+    function checkValidDimensions() {
+        const isInvalid = [internalX, internalY, internalW, internalH].some((n) => {
+            return n < 0;
+        });
+        if (isInvalid) {
+            // debugger;
+            console.log('Invalid dimensions');
+            // throw new Error('Invalid dimensions');
+        }
+    }
+
     function move(x, y) {
         clear();
         internalX += x;
         internalY += y;
         draw();
+        checkValidDimensions();
         checkoutOfBounds();
     }
 
+    function resize(x, y) {
+        clear();
+        internalW += x;
+        internalH += y;
+        draw();
+        checkValidDimensions();
+        checkoutOfBounds();
+    }
+
+    function getDimensions() {
+        return {
+            x: internalX,
+            y: internalY,
+            width: internalW,
+            height: internalH
+        };
+    }
+
+    id++;
+
     return {
+        getDimensions,
+        resize,
         draw,
         clear,
-        move
+        move,
+        id
     };
 }
 
@@ -94,27 +133,136 @@ function canvasView(...args) {
         canvasContext = canvas.getContext('2d');
     }
 
-    function createRect(width = 1, height = 1) {
-        const rect = shape(canvasContext, 25, 25, width * boxSize, height * boxSize);
+    function createRect(originX = 25, originY = 25, width = 1, height = 1) {
+        const rect = shape(canvasContext, originX, originY, width * boxSize, height * boxSize);
         rect.draw();
+        shapeMap[shape.id] = shape;
         return rect;
+    }
+
+    function growRect(...args) {
+        return resizeRect(...args, true);
+    }
+
+    function shrinkRect(...args) {
+        return resizeRect(...args);
+    }
+
+    function _resizeRect(rect, direction, grow) {
+        let x = 0, y = 0;
+        switch (direction) {
+            case 'right':
+                x = 1;
+                break;
+            case 'left':
+                x = -1;
+                break;
+            case 'up':
+                y = -1;
+                break;
+            case 'down':
+                y = 1;
+                break;
+        }
+
+        return new Promise((resolve, reject) => {
+            let i = 0;
+            const interval = setInterval(() => {
+                i++;
+                try {
+                    if (grow) {
+                        rect.resize(x, y);
+                    } else {
+                        rect.move(x, y);
+                        rect.resize(x * -1, y * -1);
+                    }
+                } catch (ex) {
+                    clearInterval(interval);
+                    reject(ex);
+                }
+                if (i === boxSize) {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, 10);
+        });
+    }
+
+
+    function resizeRect(rect, direction, grow) {
+        let moveX = 0, moveY = 0,
+            growX = 0, growY = 0;
+        if (grow) {
+            switch (direction) {
+            case 'right':
+                growX = 1;
+                break;
+            case 'left':
+                moveX = -1;
+                growX = 1;
+                break;
+            case 'up':
+                moveY = -1;
+                growY = 1;
+                break;
+            case 'down':
+                growY = 1;
+                break;
+            }
+        } else {
+            switch (direction) {
+            case 'right':
+                moveX = 1;
+                growX = -1;
+                break;
+            case 'left':
+                growX = -1;
+                break;
+            case 'up':
+                growY = -1;
+                break;
+            case 'down':
+                growY = -1;
+                moveY = 1;
+                break;
+            }            
+        }
+
+
+        return new Promise((resolve, reject) => {
+            let i = 0;
+            const interval = setInterval(() => {
+                i++;
+                try {
+                    rect.move(moveX, moveY);
+                    rect.resize(growX, growY);
+                } catch (ex) {
+                    clearInterval(interval);
+                    reject(ex);
+                }
+                if (i === boxSize) {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, 10);
+        });
     }
 
     function moveRect(rect, direction) {
         let x = 0, y = 0;
         switch (direction) {
-        case 'right':
-            x = 1;
-            break;
-        case 'left':
-            x = -1;
-            break;
-        case 'up':
-            y = -1;
-            break;
-        case 'down':
-            y = 1;
-            break;
+            case 'right':
+                x = 1;
+                break;
+            case 'left':
+                x = -1;
+                break;
+            case 'up':
+                y = -1;
+                break;
+            case 'down':
+                y = 1;
+                break;
         }
 
         return new Promise((resolve, reject) => {
@@ -135,9 +283,32 @@ function canvasView(...args) {
         });
     }
 
+    function removeOne(rect, direction) {
+        let x = 0, y = 0;
+        switch (direction) {
+        case 'right':
+            x = 1;
+            break;
+        case 'left':
+            x = -1;
+            break;
+        case 'up':
+            y = -1;
+            break;
+        case 'down':
+            y = 1;
+            break;
+        }
+        rect.move(x * boxSize, y * boxSize);
+        rect.resize(x * boxSize * -1, y * boxSize * -1);
+    }
+
     return _.merge({}, view, {
         createContainer,
         createRect,
+        growRect,
+        shrinkRect,
+        removeOne,
         moveRect
     });
 }
--- src/snake.js
+++ src/snake.js
@@ -5,37 +5,178 @@ const modes = {
     canvas: canvasView
 };
 
+let pause = false;
+
 export function init(containerEl, config) {
     if (!modes[config.mode]) {
         throw new Error(`Mode ${config.mode} is not supported`);
     }
-    const view = modes[config.mode](containerEl);
+    const view = modes[config.mode](containerEl),
+        boxSize = config.boxSize || 10;
 
-    let rect = null,
-        interval;
+    let rects = [];
 
-    function move(direction) {
-        view.moveRect(rect, direction)
-            .then(() => {
-                move(direction);
-            })
-            .catch(() => {
+    function move() {
+        if (pause) {
+            return;
+        }
+        if (rects.length === 1) {
+            const rect = rects[0].rect,
+                direction = rects[0].direction;
+            view.moveRect(rect, direction)
+                .then(() => {
+                    move();
+                })
+                .catch(() => {
 
-            });
+                });
+        } else {
+            const frontRect = rects[rects.length - 1],
+                backRect = rects[0];
+            if (!frontRect.rect) {
+                setTimeout(() => {
+                    frontRect.rect = frontRect.createSelfRect();
+                    view.removeOne(backRect.rect, backRect.direction);
+                    backRect.size--;
+                    move();
+                }, 0); // push to end of eventloop
+                return;
+            }
+            Promise.all([
+                view.growRect(frontRect.rect, frontRect.direction),
+                view.shrinkRect(backRect.rect, backRect.direction)
+            ])
+                .then(() => {
+                    backRect.size--;
+                    // console.log('[backRect]', backRect.rect.getDimensions());
+                    frontRect.size++;
+                    const size = backRect.rect.getDimensions();
+                    if (backRect.size === 0 || size.width < 0 || size.height < 0) {
+                        backRect.rect.clear();
+                        rects.shift(); // remove back rect
+
+                    }
+                    move();
+                })
+                .catch((ex) => {
+                    throw ex;
+                });
+        }
     }
 
     view.createContainer();
-    rect = view.createRect(10, 1);
+    rects.push({
+        rect: view.createRect(25, 25, 10, 1),
+        size: 10,
+        direction: 'right'
+    });
 
     document.addEventListener('keyup', function (event) {
-        const direction = keyCodes[event.keyCode]; // assume keycodes only map to directions for now
-        if (direction) {
-            clearInterval(interval);
-            move(direction);
+        const direction = keyCodes[event.keyCode],
+            lastRect = rects[rects.length - 1]; // assume keycodes only map to directions for now
+
+        function createSelfRect () {
+            const origin = deriveOrigin(lastRect.direction, direction, lastRect.rect.getDimensions());
+            return view.createRect(origin.x, origin.y, 1, 1);
+        }
+        if (direction 
+                && direction !== lastRect.direction 
+                && !opposite(lastRect.direction, direction)) {
+            console.log('[snake] allowable direction change detected:', direction);
+            rects.push({
+                createSelfRect, 
+                size: 1,
+                direction
+            });
+        }
+        if (event.keyCode === 32) {
+            pause = !pause;
+            move();
         }
     });
 
-    setTimeout(() => {
-        move('right'); // avoid CTRL+R key up messing up things
-    });
+    function opposite(lastDirection, direction) {
+        const check = {
+                'up': 'down',
+                'down': 'up',
+                'left': 'right',
+                'right': 'left'
+            },
+            checkValue = check[lastDirection] === direction;
+        console.log(`[check opposite] lastDirection: ${lastDirection}, direction: ${direction}, isOpposite: ${checkValue}`);
+        return checkValue;
+    }
+
+    function deriveOrigin(lastDirection, direction, dimensions) {
+        const {
+            x, y, width, height 
+        } = dimensions;
+
+        console.log(`[deriveOrigin] lastDirection: ${lastDirection}, direction: ${direction}`);
+        
+        if (lastDirection === 'right') {
+            if (direction === 'down') {
+                return {
+                    x: x + width - boxSize,
+                    y: y + boxSize
+                };
+            }
+            if (direction === 'up') {
+                return {
+                    x: x + width - boxSize,
+                    y: y
+                };
+            }
+        }
+
+        if (lastDirection === 'left') {
+            if (direction === 'down') {
+                return {
+                    x: x - boxSize,
+                    y: y + boxSize
+                };
+            }
+            if (direction === 'up') {
+                return {
+                    x: x - boxSize,
+                    y
+                };
+            }
+        }
+
+        if (lastDirection === 'down') {
+            if (direction === 'right') {
+                return {
+                    x: x + boxSize,
+                    y: y + height - boxSize
+                };
+            }
+            if (direction === 'left') {
+                return {
+                    x: x,
+                    y: y + height - boxSize
+                };
+            }
+        }
+
+        if (lastDirection === 'up') {
+            if (direction === 'right') {
+                return {
+                    x: x,
+                    y: y - boxSize
+                };
+            }
+            if (direction === 'left') {
+                return {
+                    x, 
+                    y: y - boxSize // ?? why
+                };
+            }
+        }
+    }
+
+    move(); // avoid CTRL+R key up messing up things
+
+    window.rects = rects;
+
 }
